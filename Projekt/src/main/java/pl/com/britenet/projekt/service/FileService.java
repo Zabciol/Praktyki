879ef20db270662a0056b13f43833f85273d1363
Zabciol <104020296+Zabciol@users.noreply.github.com>
2022-11-04 13:29:28 +0100

    Narazie jak kolwiek działa połączone

---
+++ Projekt/src/main/java/pl/com/britenet/projekt/service/FileService.java
@@ -0,0 +1,57 @@
+package pl.com.britenet.projekt.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.com.britenet.projekt.domain.File;
+
+import java.util.*;
+
+/**
+ * Service class for managing users.
+ */
+@Service
+@Transactional
+public class FileService {
+    private static final Map<String, File> files = new HashMap<>();
+
+    public FileService() {
+
+    }
+
+    List<String> fileList = new LinkedList<>();
+
+    public List<String> getList(String name){
+        for (var entry : files.entrySet()) {
+            if(entry.getKey().equals(name)){
+                fileList.add(name);
+            }
+        }
+        return fileList;
+    }
+
+
+    public static File getFile() {
+        final var file = new File();
+        file.setName("userinput");
+        return file;
+    }
+
+    public static File getFile(String name) {
+        if (files.containsKey(name)) {
+            return files.get(name);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public static void addFile(File file) {
+        files.put(file.getName(), file);
+    }
+
+    public static void deleteFile(String name) {
+        if (files.containsKey(name)) {
+            files.remove(name);
+        }
+    }
+}
--- Projekt/src/main/java/pl/com/britenet/projekt/web/rest/FileController.java
+++ Projekt/src/main/java/pl/com/britenet/projekt/web/rest/FileController.java
@@ -1,34 +1,39 @@
 package pl.com.britenet.projekt.web.rest;
 
-import java.io.File;
-import java.util.*;
-import java.util.Collections;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import pl.com.britenet.projekt.service.UserService;
-import pl.com.britenet.projekt.service.dto.UserDTO;
-import tech.jhipster.web.util.PaginationUtil;
+import pl.com.britenet.projekt.service.FileService;
 
+import java.io.File;
+import java.util.List;
+
+@RequestMapping("/api/files")
 @RestController
-@RequestMapping("/api")
-
 public class FileController {
 
-    private final Logger log = LoggerFactory.getLogger(FileController.class);
 
-    @PostMapping ("/files")
-    public ResponseEntity<String> saveFiles(@RequestParam("file")MultipartFile file) {
 
-        log.info("przyjecie pliku p romiarze: {}", file.getSize());
-        return ResponseEntity.ok("done");
+    //private final FileService ;
+
+    @GetMapping
+    public List<File> getFiles() {
+        FileService.getFile();
+        return null;
     }
+
+    @PostMapping
+    public void createFile(@RequestBody pl.com.britenet.projekt.domain.File file) {
+        FileService.addFile(file);
+
+    }
+    @DeleteMapping
+    public void  deleteFile(@RequestBody String file)
+    {
+         FileService.deleteFile(file);
+    }
+    @PutMapping
+    public void  replaceFile(@RequestBody String file)
+    {
+        FileService.getFile(file);
+    }
+
 }
